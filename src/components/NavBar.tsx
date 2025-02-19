b4dc520b1106f08dff050c2b4b313126ad41306a
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-02-19 11:21:30 -0300

    nav data and nav component

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,11 @@
+const NavBar = () => {
+  return (
+    <nav>
+      <ul>
+        <li></li>
+      </ul>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ src/data/navData.ts
@@ -0,0 +1,16 @@
+const navItems = [
+  {
+    name: "Home",
+  },
+  {
+    name: "About",
+  },
+  {
+    name: "Blog",
+  },
+  {
+    name: "Contact",
+  },
+];
+
+export { navItems };
