46dcc3adc4e9c33e3f6a0574e79c9afc11cbc21a
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-02-19 11:45:31 -0300

    theme variables, navbar styles and orbitron font

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,13 @@ import { navItems } from "../data/navData";
 
 const NavBar = () => {
   return (
-    <nav className="w-full h-16 flex items-center justify-center">
+    <nav className="w-full h-16 flex items-center justify-center font-orbitron bg-transparent">
       <ul className="flex items-center justify-center gap-2">
         {navItems.map((item) => (
-          <li key={item.id} className="decoration-0 cursor-pointer">
+          <li
+            key={item.id}
+            className="decoration-0 cursor-pointer text-text-50"
+          >
             {item.name}
           </li>
         ))}
