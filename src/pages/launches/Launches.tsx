07449810e3896cac95f1fc76189eb58aae96922a
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2025-02-20 16:38:03 -0300

    input styles

--- src/pages/launches/Launches.tsx
+++ src/pages/launches/Launches.tsx
@@ -2,6 +2,7 @@ import MainLayout from "../../layouts/MainLayout";
 import LaunchCard from "../../components/LaunchCard";
 import { useContext } from "react";
 import { ApiContext } from "../../context/apiContext";
+import { MagnifyingGlassCircleIcon } from "@heroicons/react/20/solid";
 
 const Launches = () => {
   const { upComingLaunches } = useContext(ApiContext);
@@ -15,10 +16,13 @@ const Launches = () => {
             <h1 className="text-4xl font-bold font-orbitron text-white tracking-wider">
               Upcoming Launches
             </h1>
-            <input
-              type="text"
-              className="bg-transparent text-white px-26 py-2 rounded-lg border border-white transition-all cursor-pointer text-lg font-orbitron hover:scale-105 "
-            />
+            <div className="relative">
+              <MagnifyingGlassCircleIcon className="absolute h-6 w-6 left-3 top-2.5 mr-2 text-white" />
+              <input
+                type="text"
+                className="bg-transparent text-white px-10 py-2 rounded-lg border border-white transition-all cursor-pointer text-lg font-orbitron hover:scale-105 pl-10"
+              />
+            </div>
           </div>
 
           <div className="grid grid-cols-3 gap-8 auto-rows-auto">
